a4055ed035aeee8a07da76bb28050cc42899d378
Ste <mrbarrow@hotmail.co.uk>
2025-02-14 18:55:16 +0000

    feat:Percolation.java

---
+++ percolation/Percolation.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    public static void main(String[] args) {
+
+        WeightedQuickUnionUF w = new WeightedQuickUnionUF(4);
+    }
+}
