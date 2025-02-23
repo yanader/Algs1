4c98224bbd2460b315fe5e65d3860a84ed2ebcc1
Ste <mrbarrow@hotmail.co.uk>
2025-02-23 14:08:22 +0000

    instance variables/inner class

--- queues/Deque.java
+++ queues/Deque.java
@@ -8,9 +8,23 @@ import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
 
+    // Attributes
+    private int size;
+    private Node first;
+    private Node last;
+
+    // Inner class
+    private class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+        private Node<Item> previous;
+    }
+
     // construct an empty deque
     public Deque() {
-
+        first = null;
+        last = null;
+        size = 0;
     }
 
     // is the deque empty?
@@ -20,7 +34,7 @@ public class Deque<Item> implements Iterable<Item> {
 
     // return the number of items on the deque
     public int size() {
-        return -1
+        return -1;
     }
 
     // add the item to the front
