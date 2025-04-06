848e170a66750a2eabcfd856f5fdfea849bd84ea
Ste <mrbarrow@hotmail.co.uk>
2025-04-06 14:13:51 +0100

    feat:BruteCollinearPoints

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
